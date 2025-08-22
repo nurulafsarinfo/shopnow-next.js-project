98c5871c2fa50bcef833419ea32b2a8a33f0378e
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-08-22 19:01:19 +0600

    add signup functionality in signup page

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function SignupPage() {
+    const [ form, setForm ] = useState({ name: "", email: "", password: "" });
+    const router = useRouter();
+
+    const handlechange = (e) => {
+        const { name, value } = e.target;
+        setForm((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const res = await fetch("/api/auth/signup", {
+            method: "POST",
+            body: JSON.stringify(form),
+        });
+
+        if(res.ok) {
+            router.push("/login");
+        }else {
+            alert('Signup failed');
+        }
+    }  
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" onChange={handlechange}/>
+
+            <input type="email" placeholder="Email" onChange={handlechange}/>
+
+            <input type="password" placeholder="Password" onChange={handlechange}/>
+
+            <button type="submit">Sign Up</button>
+
+        </form>
+    )
+}
