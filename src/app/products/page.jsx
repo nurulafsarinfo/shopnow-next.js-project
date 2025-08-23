396cbc341a956d980c8be6df35cb016dda78b385
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-08-23 12:59:35 +0600

    change site url local to production

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,30 +1,40 @@
-"use client";
-import { listProducts } from "../lib/products"
+import Link from 'next/link'; 
+import { listProducts } from "../lib/products";
+
+export const dynamic = 'force-dynamic';
 
 export default async function ProductPage() {
+    // fetch data from listProducts function of product.js 
     const products = await listProducts();
 
     return (
         <div className="p-6">
-            <h1 className="text-2xl text-center font-bold mb-6">Our Products</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {
-                    products.map((product) => (
-                        <div key={product._id} className="border rounded-lg shadow p-4 ">
-                            <img src={product.images[0]} alt={product.name} className="w-full h-48 object-cover rounded" />
-                            <h2 className="text-lg font-semibold mt-2">{product.name}</h2>
-                            <p className="text-gray-500">{product.brand}</p>
-                            <div className="flex gap-2 items-baseline">
-                                <p className="text-xl font-bold mt-2">$ {product.discountPrice}</p>
-                                <p className="line-through mt-2 text-red-500 ">$ {product.price}</p>
-                                
-                            </div>
-
-                            <a href={`/products/${product.slug}`} className="text-[#33d6ef] hover:underline block mt-2 border text-center py-1 rounded-sm">View Details</a>
-                        </div>     
-                    ))
-                }
+            <h1 className="mb-6 text-center text-2xl font-bold">Our Products</h1>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {products.map((product) => (
+                    <div key={product._id.toString()} className="rounded-lg border p-4 shadow">
+                        <img 
+                            src={product.images[0]} 
+                            alt={product.name} 
+                            className="h-48 w-full rounded object-cover" 
+                        />
+                        <h2 className="mt-2 text-lg font-semibold">{product.name}</h2>
+                        <p className="text-gray-500">{product.brand}</p>
+                        <div className="flex items-baseline gap-2">
+                            <p className="mt-2 text-xl font-bold">$ {product.discountPrice}</p>
+                            <p className="mt-2 text-red-500 line-through">$ {product.price}</p>
+                        </div>
+                        
+                        {/* use Link for Client-side navigation */}
+                        <Link 
+                            href={`/products/${product.slug}`} 
+                            className="mt-2 block rounded-sm border py-1 text-center text-[#33d6ef] hover:underline"
+                        >
+                            View Details
+                        </Link>
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
